8f5cbbf124aa9307f576e940f032b6f9d35823d9
Sr-Chandan-Sahu <cs154896@gmail.com>
2025-07-08 01:54:25 +0530

    feat: AddEmployeeFormScreen, Employee list and Home Screen

--- Frontend/app/(tabs)/_layout.tsx
+++ Frontend/app/(tabs)/_layout.tsx
@@ -2,44 +2,46 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Home, Users } from 'lucide-react-native';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
-            // Use a transparent background on iOS to show the blur effect
             position: 'absolute',
+            backgroundColor: '#ffffff',
           },
-          default: {},
+          android: {
+            backgroundColor: '#ffffff',
+            height: 60,
+          },
+          default: {
+            backgroundColor: '#ffffff',
+            height: 90,
+            borderTopWidth: 0,
+          },
         }),
+        tabBarActiveTintColor: '#8B5CF6',
+        tabBarInactiveTintColor: '#000000',
       }}>
       <Tabs.Screen
-        name="index"
+        name="home"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => <Home size={28} color={color} />,
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="employees"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Employees',
+          tabBarIcon: ({ color }) => <Users size={28} color={color} />,
         }}
       />
-    </Tabs>
+    </Tabs> 
   );
 }
---
+++ Frontend/app/add-employee.tsx
@@ -0,0 +1,10 @@
+import AddEmployeeScreen from '@/screens/AddEmployeeFormScreen';
+import React from 'react';
+
+const AddEmployee = () => {
+  return (
+    <AddEmployeeScreen/>
+  )
+}
+
+export default AddEmployee;
