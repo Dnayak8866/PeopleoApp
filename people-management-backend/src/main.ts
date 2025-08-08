b8cb008fb7e0410322f1632c7bb9c9c550c53c44
Dnayak8866 <nayakdharmesh2@gmail.com>
2025-08-08 12:46:21 +0530

    added swagger

--- people-management-backend/src/main.ts
+++ people-management-backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Swagger config
+  const config = new DocumentBuilder()
+    .setTitle('People Management API')
+    .setDescription('API documentation for People Management Backend')
+    .setVersion('1.0')
+    .addBearerAuth() // If you have JWT auth
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
